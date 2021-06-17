f9e0afa708f4b78c9195010cbef3a59f47847bce
lakshaa <lakssepaul@gmail.com>
2021-06-17 08:57:09 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ debugger
           if (principal == 0 || principal < 0){
          alert("Enter a positive number");
          }
+
      else {
+         
      document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 }
         
