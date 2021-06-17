6eb5f07561b3bbaa6eca4e10e3e416c55c2248bc
lakshaa <lakssepaul@gmail.com>
2021-06-17 08:52:48 +0000

    Initial commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
      var interest = principal * years * rate /100;
      var year = new Date().getFullYear()+parseInt(years);
 debugger
-          if (principal == 0){
+          if (principal == 0 || principal < 0){
          alert("Enter a positive number");
          }
      else {
